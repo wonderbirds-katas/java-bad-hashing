d6e7cb032497c695bb457ef617b4543d8895cfe8
Stefan Boos <kontakt@boos.systems>
2025-03-13 05:47:07 +0100

    feat: B.A.D. hash satisfies given tests

--- src/main/java/systems/boos/BadWorkHash.java
+++ src/main/java/systems/boos/BadWorkHash.java
@@ -1,8 +1,50 @@
 package systems.boos;
 
+import com.codepoetics.protonpack.StreamUtils;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class BadWorkHash {
 
+  private BadWorkHash() {}
+
   public static String hashString(String toHash) {
-    //Eye of the tiger!
+    // pad with "a" until length is 32 characters
+    var padding = "a".repeat(Math.max(0, 32 - toHash.length()));
+    var padded = toHash + padding;
+
+    var truncated = padded.substring(0, 32);
+
+    // convert to list of ascii codes, sort it descending
+    List<Integer> ascii = truncated
+      .chars()
+      .boxed()
+      .collect(Collectors.toList());
+
+    // Convert the values to strings before sorting
+    List<String> valueStrings = ascii
+      .stream()
+      .map(Object::toString)
+      .collect(Collectors.toList());
+
+    // Sort the list lexicographically descending by the values
+    Collections.sort(valueStrings);
+    Collections.reverse(valueStrings);
+
+    // add the index of each element to the element itself
+    List<Long> withIndexAdded = StreamUtils.zipWithIndex(valueStrings.stream())
+      .map(x -> x.getIndex() + Integer.parseInt(x.getValue()))
+      .toList();
+
+    // Project to hexadecimal numbers and convert to string
+    String result = withIndexAdded
+      .stream()
+      .map(x -> x % 16)
+      .map(x -> (char) (x < 10 ? '0' + x : 'A' + x - 10))
+      .map(Object::toString)
+      .reduce("", (acc, ch) -> acc + ch);
+
+    return result;
   }
 }
--- src/test/java/systems/boos/BadWorkHashTest.java
+++ src/test/java/systems/boos/BadWorkHashTest.java
@@ -7,11 +7,60 @@ import org.junit.jupiter.api.Test;
 class BadWorkHashTest {
 
   @Test
-  void exampleTestCases() {
+  void hashString_1LetterA() {
     assertEquals(
       "123456789ABCDEF0123456789ABCDEF0",
       BadWorkHash.hashString("a")
     );
+  }
+
+  @Test
+  void hashString_1LetterB() {
+    assertEquals(
+      "223456789ABCDEF0123456789ABCDEF0",
+      BadWorkHash.hashString("b")
+    );
+  }
+
+  @Test
+  void hashString_32LettersAllMappingTo0() {
+    // "ABCDEFGHIJKLMNOPQRSTUVWXYZdefghi"                       = input
+    // ..................................                       = ascii
+    // "ZYX.............C|.B|.A|...i|...h|...g|...f|...e|...d"  = lexicographically sorted, reversed
+    // 90, 89, 87, ...... 64,65, 105, 104, 103, 102, 101, 100   = ascii
+    // 90, 90, 90, ....., 90, 90, 131, 130, ...                += index
+    // 10, ...                  ,   3, ...                     %= 16
+    //  A,  A, ...             A,   3, ...                      = converted to hex
+    assertEquals(
+      "AAAAAAAAAAAAAAAAAAAAAAAAAA333333",
+      BadWorkHash.hashString("ABCDEFGHIJKLMNOPQRSTUVWXYZdefghi")
+    );
+  }
+
+  @Test
+  void hashString_33Letters_lastGetsRemoved() {
+    assertEquals(
+      "AAAAAAAAAAAAAAAAAAAAAAAAAA333333",
+      BadWorkHash.hashString("ABCDEFGHIJKLMNOPQRSTUVWXYZdefghiZ")
+    );
+  }
+
+  @Test
+  void exampleTestCases() {
+    /*
+                                                                1                                                 2                                                 3
+                   1    2    3    4    5    6    7    8    9    0    1    2    3    4    5    6    7    8    9    0    1    2    3    4    5    6    7    8    9    0    1    2
+               "   a    p    p    l    e         j    u    i    c    e"
+               [  97, 112, 112, 108, 101,  32, 106, 117, 105,  99, 101,  97,  97,  97,  97,  97,  97,  97,  97,  97,  97,  97,  97,  97,  97,  97,  97,  97,  97,  97,  97,  97 ]
+               [  99,  97,  97,  97,  97,  97,  97,  97,  97,  97,  97,  97,  97,  97,  97,  97,  97,  97,  97,  97,  97,  97,  97,  32, 117, 112, 112, 108, 106, 105, 101, 101 ]
+
+                                                                     1                                                 2                                                 3
+                   0    1    2    3    4    5    6    7    8    9    0    1    2    3    4    5    6    7    8    9    0    1    2    3    4    5    6    7    8    9    0    1
+               [  99,  98,  99, 100, 101, 102, 103, 104, 105, 106, 107,  ..............................................., 118, 119,  55, 131, ....................136, 131, 132 ]
+               [   3,   2,   3, ........................................................................................,   6,   7,   7,   3, ......................8,   3,   4]
+
+               ...
+       */
     assertEquals(
       "323456789ABCDEF012345677D9A76634",
       BadWorkHash.hashString("apple juice")
