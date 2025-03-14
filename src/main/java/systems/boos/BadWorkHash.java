b94c7aa757164b7c76894c5bc5c1fd54a7c7afd9
Stefan Boos <kontakt@boos.systems>
2025-03-14 05:56:21 +0100

    test: show how the hash can be broken

--- src/main/java/systems/boos/BadWorkHash.java
+++ src/main/java/systems/boos/BadWorkHash.java
@@ -10,6 +10,15 @@ public class BadWorkHash {
 
   private BadWorkHash() {}
 
+  /**
+   * Calculate hash code for given string.
+   * <p>
+   * This is a solution to the
+   * <a href="https://www.codewars.com/kata/59678a814e686ba17b000079">Codewars challenge B.A.D. Hashing</a>.
+   * <p>
+   * @param toHash input string for calculating the hash
+   * @return hash code of the input string as hex string
+   */
   public static String hashString(String toHash) {
     // Pad with "a" until length is 32 characters
     var padding = "a".repeat(Math.max(0, 32 - toHash.length()));
--- src/test/java/systems/boos/BadWorkHashTest.java
+++ src/test/java/systems/boos/BadWorkHashTest.java
@@ -46,7 +46,7 @@ class BadWorkHashTest {
   }
 
   @Test
-  void exampleTestCases() {
+  void codewarsExampleTestCases() {
     /*
                                                                 1                                                 2                                                 3
                    1    2    3    4    5    6    7    8    9    0    1    2    3    4    5    6    7    8    9    0    1    2    3    4    5    6    7    8    9    0    1    2
@@ -69,5 +69,63 @@ class BadWorkHashTest {
       "22334567893453234456645544344434",
       BadWorkHash.hashString("this is a very long test okay bye wow cya")
     );
+  }
+
+  /**
+   * Breaking the hash: Fill with 'a' until 32 characters reached. Then add numbers > 99000 to the end to produce the
+   * desired hash
+   * <p>
+   * To break the hash means to counter its purpose. Some purposes of a hash are:
+   * <p>
+   * Prevent password disclosure
+   * <p>
+   * A one way function shall be used to hash passwords. It shall be easy to calculate the hash, but very hard to
+   * construct a value that results in the hash.
+   * <p>
+   * Map from an input to a numeric key in a lookup table (hash table)
+   * <p>
+   * The hash shall evenly distribute input values to numbers serving as array indexes for lookup tables. The hash
+   * function must be fast and collisions shall be avoided.
+   * <p>
+   * From these two scenarios, there the following weaknesses are possible:
+   * <p>
+   * - construct an input value, which results in a given hash, i.e. calculate a valid password
+   * - construct collisions of the hash function, i.e. calculate a large number of ijnputs which produce the same
+   *   output.
+   * <p>
+   * Seeing the given B.A.D. hash function, both is quite simple (see tests). All operations of the hash function
+   * can be reversed easily.
+   */
+  @Test
+  void breakHash() {
+    var password = "SomeFancyPassword";
+    var hash = BadWorkHash.hashString(password);
+    assertEquals("323456789ABCDEF01253AEDABB9AAA33", hash);
+
+    // simple way of producing a collision: just shuffle the input characters
+    var collision = "PasswordSomeFancy";
+    assertEquals(hash, BadWorkHash.hashString(collision));
+    /*
+      Forge a collision from a known hash
+
+      Desired hash:
+      323456789ABCDEF01253AEDABB9AAA33 = desired hash
+      hex:
+      [   3,   2,   3,   4,   5,   6,   7,   8,   9,   A,   B,   C,   D,   E,   F,   0,   1,   2,   5,   3,   A,   E,   D,   A,   B,   B,   9,   A,   A,   A,   3,   3 ]
+      decimal:
+      [   3,   2,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12,  13,  14,  15,   0,   1,   2,   5,   3,  10,  14,  13,  10,  11,  11,   9,  10,  10,  10,   3,   3 ]
+
+      Indices:
+                                                            1                                                 2                                                 3
+          0    1    2    3    4    5    6    7    8    9    0    1    2    3    4    5    6    7    8    9    0    1    2    3    4    5    6    7    8    9    0    1
+
+      Subtract indices; If the result gets < 0, then add 16 until result is >= 0
+      [   3,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   3,   0,   6,   9,   7,   3,   3,   2,  15,  15,  14,  13,   5,   4 ]
+      Convert to an ASCII character by adding 'A' (=65)
+      [  68,  66,  66,  66,  66,  66,  66,  66,  66,  66,  66,  66,  66,  66,  66,  66,  66,  66,  68,  65,  71,  74,  72,  68,  68,  67,  80,  80,  79,  78,  70,  69 ]
+      Fix sorting by starting from the back and adding 16 until the array is sorted descending
+      ... better do that programmatically
+      Collision:
+     */
   }
 }
